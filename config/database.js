d7dffe0a451ffb1327bc50a8fb25c45cf168b549
Raunak Gayen <rnk.express@gmail.com>
2022-10-24 03:10:55 +0530

    made ready for deployment

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const parse = require('pg-connection-string').parse;
+module.exports = ({ env }) => {
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'sqlite',
+        connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  }
+};
