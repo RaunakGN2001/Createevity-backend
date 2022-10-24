5fb81592bb251edfde70a7a16c1ba7b19767593c
Raunak Gayen <74176824+RaunakGN2001@users.noreply.github.com>
2022-10-24 11:58:53 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+//     keys: env.array('APP_KEYS'),
+       keys: ['QmbS9TivWOPeTOZDQxftEw==','aof5t2vUA2Kmi6OnQ4D7Cw==','ggk6QFoFn96OZNFJRb+Ugw==','LxhzLbk3MSjf0K+5IIMPNA=='],
   },
 });
